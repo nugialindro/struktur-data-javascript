5dd270a97435b49b7f99a4fe016b86505104d069
nugialindro <nugialindro@gmail.com>
2022-03-16 03:26:56 +0700

    Penambahan object dan array

--- main.js
+++ main.js
@@ -32,3 +32,43 @@ console.log(supplyBurn)
 
 
 
+// object literal //
+const biTok = {
+    namaToken: "Binar Token",
+    detailToken: {
+    maxSupply: 10000,
+    initialSupply: 5000
+    },
+    hargaToken: [
+        "IDO $0.33",
+        "Listing $0.4"
+    ]
+}
+
+console.log(biTok.namaToken)
+console.log(`Max Supply ${biTok.namaToken} adalah ${biTok["detailToken"]["maxSupply"]}`)
+console.log(`Initial Supply ${biTok.namaToken} adalah ${biTok["detailToken"]["initialSupply"]}`)
+
+
+// array dalam Object
+biTok.hargaToken.forEach(function(item) {
+        console.log(`Harga ${biTok.namaToken} saat ${item}`)
+        })
+
+// array literal //
+const fiturBitokLama = ["Burn", "Fee", "Tax"];
+const fiturBitokBaru = [
+    "Burn",
+    "Fee",
+    "Tax",
+    "NFT"
+];
+
+console.log(`Fitur Lama ${biTok.namaToken} ada ${fiturBitokLama.length}`)
+console.log(`yaitu ${fiturBitokLama}`)
+
+console.log(`Fitur Baru ${biTok.namaToken} ada ${fiturBitokBaru.length}`)
+console.log(`yaitu ${fiturBitokBaru}`)
+
+const arrays =[fiturBitokLama, fiturBitokBaru]
+console.log(arrays)
