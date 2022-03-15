1630f6c6fe029fb9b8b806d4c09c04ce2bf7bd49
nugialindro <nugialindro@gmail.com>
2022-03-16 01:58:41 +0700

    Tipe Data Javascript

---
+++ main.js
@@ -0,0 +1,34 @@
+// tipe data //
+const namaToken = "BiTok";
+const jaringankToken = "ETH";
+
+console.log(`Dear Binarian, token ${namaToken} adalah token yang berada dijaringan ${jaringankToken}. Jangan sampai salah ya!`);
+// Dear Binarian, token BiTok adalah token yang berada dijaringan ETH. Jangan sampai salah ya!
+
+const hargaIDO = 0.33;
+const maxAlokasi = 100;
+const jumlahTokenIDO = maxAlokasi / hargaIDO;
+
+console.log(`Dear Binarian, jumlah token yang anda dapat dari IDO dengan alokasi maksimal adalah ${jumlahTokenIDO}`)
+// Dear Binarian, jumlah token yang anda dapat dari IDO dengan alokasi maksimal adalah 303.030303030303
+
+const tokenDikunciSatuTahun = true;
+const tokenDikunciSatuBulan = false;
+
+console.log(tokenDikunciSatuTahun)
+console.log(tokenDikunciSatuBulan)
+// true
+// false
+
+const pleTok = null;
+
+console.log(pleTok)
+// null
+
+let supplyBurn;
+
+console.log(supplyBurn)
+// undefined
+
+
+
